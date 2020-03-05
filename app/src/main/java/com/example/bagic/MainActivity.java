7b82020792c447d6a5fccc8ddcfdb2ba6d349131
namsu <niceguy0825@naver.com>
2020-03-05 17:18:49 +0900

    Parcelable과 Serializable 비교
    Parcelable이 데이터 전달 시간에 있어, Serializable 보다 빠르다.

--- app/src/main/java/com/example/bagic/MainActivity.java
+++ app/src/main/java/com/example/bagic/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.bagic;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.bt_send);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+
+                MovieParcelable parcelable = new MovieParcelable("하우스오브카드", 9.5);
+                intent.putExtra("ParcelableObject", parcelable);
+
+                MovieSerializable serializable = new MovieSerializable("워킹데드", 9);
+                intent.putExtra("SerializableObject", serializable);
+
+                startActivity(intent);
+            }
+        });
     }
 }
+
+// https://thepassion.tistory.com/276
+// https://www.charlezz.com/?p=823
