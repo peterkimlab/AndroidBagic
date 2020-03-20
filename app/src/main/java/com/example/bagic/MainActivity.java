577ba9275743c5c122c136b15c0e42eada852679
namsu <niceguy0825@naver.com>
2020-03-20 16:17:50 +0900

    Handler 사용하는 이유

--- app/src/main/java/com/example/bagic/MainActivity.java
+++ app/src/main/java/com/example/bagic/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.bagic;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView clockTextView;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clockTextView = findViewById(R.id.clock);
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Calendar cal = Calendar.getInstance();
+
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                String strTime = sdf.format(cal.getTime());
+
+                clockTextView = findViewById(R.id.clock);
+                clockTextView.setText(strTime);
+            }
+        };
+
+        class ClockThread implements Runnable {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    handler.sendEmptyMessage(0);
+                }
+            }
+        }
+
+        ClockThread clockThread = new ClockThread();
+        Thread t = new Thread(clockThread);
+        t.start();
     }
 }
